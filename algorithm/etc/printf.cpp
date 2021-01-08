2e804108fd6e5bc97d93d28f2c9711181d2d5878
nrshin <nr8122@gmail.com>
2021-01-08 10:47:33 +0900

    2021/01/08...

--- algorithm/etc/printf.cpp
+++ algorithm/etc/printf.cpp
@@ -1,55 +1,74 @@
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+//socket
+#include <sys/socket.h>
+//sockaddr_in
+#include <netinet/in.h>
+//inet_ntoa
+#include <arpa/inet.h>
+//close
+#include <unistd.h>
+
+#define PORT 12346
+#define BUF_SIZE 1024
+
+int main(){
+    int socket_fd,accepted_fd;
+    struct sockaddr_in host_addr, client_addr;
+    socklen_t size;
+    int recv_length;
+    char buffer[BUF_SIZE];
+
+    socket_fd=socket(PF_INET,SOCK_STREAM,0);
+
+    host_addr.sin_family=AF_INET;
+    host_addr.sin_port=htons(PORT);
+    host_addr.sin_addr.s_addr=0;
+    memset(&(host_addr.sin_zero),0,8);
+
+    bind(socket_fd,(struct sockaddr *)&host_addr,sizeof(struct sockaddr));
+
+    listen(socket_fd,3);
+
+    while(1){
+            size=sizeof(struct sockaddr_in);
+            accepted_fd=accept(socket_fd,(struct sockaddr *)&client_addr,&size);
+
+            send(accepted_fd,"Connected",10,0);
+            printf("Client Info : IP %s, Port %d\n", inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
+
+            recv_length=recv(accepted_fd,&buffer,BUF_SIZE,0);
+            while(recv_length>0){
+                    printf("From Client : %s\n",buffer);
+                    recv_length=recv(accepted_fd,&buffer,BUF_SIZE,0);
+            }
+
+            close(accepted_fd);
+    }
+    return 0;
+
+}
+*/
+
+
+
 #include <iostream>
-#include "pthread/pthread.h"
 
-using namespace std;
-
-class A{
+class Test{
     public:
-        A(){};
-        ~A(){};
-        
-        static void * process(void* data);
-        void asd();
-        void sdf();
-        int num() const;
-    private:
-        int a;
+        Test();
+        ~Test();
+        bool start();
 };
-int A::num() const{
-    printf("%d\n", a);
-}
-void A::asd(){
 
-    pthread_t t;
-    pthread_create(&t, NULL, process, (void*) this);
-    pthread_detach(t);
-    
-    pthread_t t1;
-    A* b = new A;
-    pthread_create(&t1, NULL, process, (void*) b);
-    pthread_detach(t1);
+bool Test::start(){
+    printf("start!\n");
+    return true;
 }
-void A::sdf(){
-    printf("123\n");
-    num();
-}
-void * A::process(void* data){
-    int temp =1 ;
-
-    A* d = (A*)data;
-    
-    while(1){
-        d->a = temp;
-        temp++;
-        d->sdf();
-    }
-        
-}
-int main() {
-    A* a = new A ;
-    a->sdf();
-    a->asd();
-    while(1){
-
-    }
+int main(){
+    Test t;
+    t.start();
 }
