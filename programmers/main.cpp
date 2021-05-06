12a79643dea45f5d6402d9bb33aa72159239d002
nrshin <nr8122@gmail.com>
2021-05-06 11:51:58 +0900

    - check

--- programmers/main.cpp
+++ programmers/main.cpp
@@ -1,11 +1,36 @@
-#include <map>
-#include <string>
+#include <iostream>
+#include <vector>
+#include <math.h>
+
 using namespace std;
-int main()
+
+
+int make_SegmentTree(int start, int end, int nodeIndex, vector<int>& array, vector<int>& segmentTree)
 {
-    map<string, int> m;
-    m["str"]+=1;
+    if(start == end) return segmentTree[nodeIndex] = array[start];
 
-    printf("%d\n", m["str"]);
+    int middle = (start+end)/2;
+    
+    int leftSum = make_SegmentTree(start, middle, nodeIndex*2, array, segmentTree);
+    int rightSum = make_SegmentTree(middle, end, nodeIndex*2+1, array, segmentTree);
+    segmentTree[nodeIndex] = leftSum+rightSum;
 
+    return segmentTree[nodeIndex];
 }
+vector<int> init(vector<int> array)
+{
+    int height = ceil(log2(array.size()));
+    int size = (1 << (height+1));
+    vector<int> segmentTree(size);
+    make_SegmentTree(0, array.size(), 1, array, segmentTree);
+
+    return segmentTree;
+}
+
+int main()
+{   
+    vector<int> array = { 1, 3, 4, 6, 7};
+
+    vector<int> segmentTree = init(array);
+
+}   
